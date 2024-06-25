1788a8302d5528f38b2a61df4ebfa0d82a674caf
simon <155122848+simonpotel@users.noreply.github.com>
2024-06-25 16:20:04 +0200

    🐛 forgot to edit get_discord_config

--- discord_bot/configs.py
+++ discord_bot/configs.py
@@ -4,5 +4,5 @@ def get_discord_config():
     """
     Function to read bot configuration from bot_config.json
     """
-    with open("configs/discord.json", "r") as f:
+    with open("configs/discord_bot.json", "r") as f:
         return json.load(f)
