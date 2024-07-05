41c86744fba082f908f0ede88e847bf8b10ef5fb
simon <155122848+simonpotel@users.noreply.github.com>
2024-07-05 12:07:54 +0200

    🗃️ edit structure of table simulations in db (p key)

--- src/db/manager.py
+++ src/db/manager.py
@@ -9,25 +9,27 @@ class DatabaseManager:
 
     def create_tables(self):
         self.db_cursor.execute('''CREATE TABLE IF NOT EXISTS simulations (
+                                  simulation_name TEXT,
+                                  start_ts TEXT,
+                                  end_ts TEXT,
+                                  positions TEXT,
+                                  PRIMARY KEY (simulation_name, start_ts, end_ts))''')
+        self.db_cursor.execute('''CREATE TABLE IF NOT EXISTS positions (
                                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                                   simulation_name TEXT,
                                   start_ts TEXT,
                                   end_ts TEXT,
-                                  positions TEXT)''')
-        self.db_cursor.execute('''CREATE TABLE IF NOT EXISTS positions (
-                                  id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                  simulation_id INTEGER,
                                   pair TEXT,
                                   buy_date TEXT,
                                   buy_price REAL,
                                   sell_date TEXT,
                                   sell_price REAL,
-                                  FOREIGN KEY(simulation_id) REFERENCES simulations(id))''')
+                                  FOREIGN KEY(simulation_name, start_ts, end_ts) REFERENCES simulations(simulation_name, start_ts, end_ts))''')
         self.db_connection.commit()
 
     def save_simulation_data(self, simulation_name, start_ts, end_ts, positions):
         try:
-            self.db_cursor.execute('''INSERT INTO simulations (simulation_name, start_ts, end_ts, positions) 
+            self.db_cursor.execute('''INSERT OR REPLACE INTO simulations (simulation_name, start_ts, end_ts, positions) 
                                       VALUES (?, ?, ?, ?)''', 
                                    (simulation_name, start_ts, end_ts, json.dumps(positions)))
             self.db_connection.commit()
@@ -35,8 +37,8 @@ class DatabaseManager:
         except sqlite3.Error as e:
             print(f"An error occurred while saving simulation data: {e}")
 
-    def save_position(self, simulation_id, pair, buy_date, buy_price, sell_date, sell_price):
-        self.db_cursor.execute('''INSERT INTO positions (simulation_id, pair, buy_date, buy_price, sell_date, sell_price) 
-                                  VALUES (?, ?, ?, ?, ?, ?)''', 
-                               (simulation_id, pair, buy_date, buy_price, sell_date, sell_price))
+    def save_position(self, simulation_name, start_ts, end_ts, pair, buy_date, buy_price, sell_date, sell_price):
+        self.db_cursor.execute('''INSERT INTO positions (simulation_name, start_ts, end_ts, pair, buy_date, buy_price, sell_date, sell_price) 
+                                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
+                               (simulation_name, start_ts, end_ts, pair, buy_date, buy_price, sell_date, sell_price))
         self.db_connection.commit()
