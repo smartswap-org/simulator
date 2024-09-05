82acdbb6e3308069293600250e39816eebfd2489
simon <155122848+simonpotel@users.noreply.github.com>
2024-09-05 11:03:01 +0200

    edit simulates: introduce index find and open_positions db req

--- src/simulation/simulates.py
+++ src/simulation/simulates.py
@@ -11,9 +11,25 @@
 
 import aiohttp
 from src.discord.configs import get_simulations_config
+from src.api.fetch import fetch_ohlcv_from_api
+from datetime import datetime
 
 async def simulates(simulator):
-    #async with aiohttp.ClientSession() as session:
-    #    simulates_config = get_simulations_config()  # get simulations configuration
-    #    for simulation_name, simulation in simulates_config.items():
-    return 
+    async with aiohttp.ClientSession() as session:
+        simulates_config = get_simulations_config()  # get simulations configuration
+        for simulation_name, simulation in simulates_config.items():
+            most_recent_date = simulator.positions.get_most_recent_date(simulation_name)
+            #if most_recent_date:
+            #    start_ts = most_recent_date
+            #else:
+            #    start_ts = simulation.get('api')['start_ts']
+            start_ts = simulation.get('api')['start_ts']
+            data = await fetch_ohlcv_from_api(simulation)
+            pairs_list = simulation.get('api')['pairs_list']
+            for idx, pair_data in enumerate(data):
+                pair_name = pairs_list[idx]
+                open_positions = simulator.positions.get_open_positions_by_pair(simulation_name, pair_name)
+                max_index = simulator.positions.get_max_index_for_pair(simulation_name, pair_name)
+                start_index = 0
+                if max_index != None: start_index = max_index
+    return
